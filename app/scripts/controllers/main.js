9879467e486f1ced5f17e2f77c4c78da12a11286
Joakim Wimmerstedt <joakim.wimmerstedt@gmail.com>
2013-08-06 15:23:17 +0200

    Added example songs, started on main controller

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,13 @@
 'use strict';
 
 angular.module('hakanlanar')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, $http) {
+    $http.get('data/songs.json').success(function(data) {
+        $scope.songs = data[0].tracks;
+
+        for (var i = $scope.songs.length - 1; i >= 0; i--) {
+            console.log($scope.songs[i]);
+        };
+
+    });
   });
