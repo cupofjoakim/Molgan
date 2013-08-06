315f5b9ef627b57736cf36ab301a299b6e30c596
Joakim Wimmerstedt <joakim.wimmerstedt@gmail.com>
2013-08-06 17:02:00 +0200

    Added song handling in Controller

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,13 +1,27 @@
 'use strict';
 
 angular.module('hakanlanar')
-  .controller('MainCtrl', function ($scope, $http) {
+.controller('MainCtrl', function ($scope, $http) {
     $http.get('data/songs.json').success(function(data) {
-        $scope.songs = data[0].tracks;
+        var randoms = [],
+            arr = [];
 
-        for (var i = $scope.songs.length - 1; i >= 0; i--) {
-            console.log($scope.songs[i]);
-        };
+        while(arr.length < 10){
+            var randomnumber=Math.ceil(Math.random()*10)
+            var found=false;
+
+            for(var i=0;i<arr.length;i++){
+                if(arr[i]==randomnumber){found=true;break}
+            }
+            if(!found)arr[arr.length]=randomnumber;
+        }
+        console.log(arr);
+
+        for (var i=0;i<arr.length;i++){ // Slumpa tio låtar
+            randoms.push(data[0].tracks[i]);
+        }
+
+        $scope.songs = randoms;
 
     });
-  });
+});
