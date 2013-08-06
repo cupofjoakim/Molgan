2c1878f8267b0befa0e9ba65ecf51f77c7de89d9
Darth Vader <prakashkc@Prakash-Khatri-Chhetris-MacBook-Pro.local>
2013-08-06 16:58:51 +0200

    Added timer + progressbar and css-transition

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,8 +3,20 @@
 angular.module('hakanlanar')
   .controller('MainCtrl', function ($scope) {
     $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+      $scope.time = time
     ];
+    var timer, time = 10, posX = 0, timeBar = $('.timebar'), timeBarWidth = timeBar.width(), score = 10;
+    countDown();
+    function countDown(){
+    	 timer = setTimeout(function() {
+			posX -= timeBarWidth / 10;
+			timeBar.css({'-webkit-transform': 'translateX('+ posX +'px)'});
+    		countDown();
+        },1000);
+    	if (time <= 0) {
+    		alert('GAME OVER');
+    		// place result route here
+    	}
+    }
   });
+
