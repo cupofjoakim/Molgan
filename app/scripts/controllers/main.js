d9f7e2c5d01418c6c6cce83b1967a10cdb9a12d7
Darth Vader <prakashkc@Prakash-Khatri-Chhetris-MacBook-Pro.local>
2013-08-06 17:19:23 +0200

    Removed duplicated code

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,21 +10,6 @@ angular.module('hakanlanar')
     $scope.awesomeThings = [
       $scope.time = time
     ];
-    var timer, time = 10, posX = 0, timeBar = $('.timebar'), timeBarWidth = timeBar.width(), score = 10;
-    countDown();
-    function countDown(){
-    	 timer = setTimeout(function() {
-    	 	time -= 1;
-			posX -= timeBarWidth / 10;
-			timeBar.css({'-webkit-transform': 'translateX('+ posX +'px)'});
-    		countDown();
-        },1000);
-    	if (time === 0) {
-    		alert('GAME OVER');
-    		// place result route here
-    	}
-    }
-
   });
         while(arr.length < 10){
             var randomnumber=Math.ceil(Math.random()*10)
