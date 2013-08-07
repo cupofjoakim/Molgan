35e06f9dc9bf5e88373e3b27f6193cf643fb0143
Darth Vader <prakashkc@Prakash-Khatri-Chhetris-MacBook-Pro.local>
2013-08-07 11:01:33 +0200

    Fixed more UI for intro and hoverstates for choices.

--- app/scripts/controllers/intro.js
+++ app/scripts/controllers/intro.js
@@ -2,4 +2,7 @@
 
 angular.module('hakanlanar')
   .controller('IntroCtrl', function ($scope) {
+  	var speechBubble = $('.intro-container').find('div');
+
+  	speechBubble.addClass('slideUp');
   });
